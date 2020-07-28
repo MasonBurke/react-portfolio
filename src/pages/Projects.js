ccae054d72965685bc1a7d3911529d5c97f3434a
Mason Burke <masonburke@rocketmail.com>
2020-07-28 12:41:46 -0600

    text added

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -18,7 +18,8 @@ function Projects() {
 
 <p style={{wordWrap:' break-word' ,flex:1, justifyContent: 'center', alignItems: 'center'}}> 
 For this project we wanted to create a way for users to find cheap flights, read postings from
- other members on how they liked the place and to find out about how much they spent. You can also log your own trips here.  </p>
+ other members on how they liked the place and to find out about how much they spent. You can also log your own trips here. This was built
+ using the MERN stack </p>
  <p> <a href='https://github.com/MasonBurke/cheap-travel'> The Code </a>||
   <a href="https://desolate-citadel-71436.herokuapp.com/"> The Site </a> </p><hr/>
 
@@ -28,7 +29,8 @@ For this project we wanted to create a way for users to find cheap flights, read
 
 <br/>
 <p style={{wordWrap:' break-word' ,flex:1, justifyContent: 'center', alignItems: 'center'}}> 
-This project used a weather API to pull weather updates from any area you search </p>
+This project used a weather API to pull weather updates from any area you search. Technologies used include
+HTML, CSS, Javascript, and local storage. </p>
 <p> <a href='https://github.com/MasonBurke/weather-app'> The Code </a>||
 <a href="https://masonburke.github.io/weather-app/"> The Site </a> </p>
 
@@ -38,7 +40,8 @@ This project used a weather API to pull weather updates from any area you search
 </Row>
 <br/>
 <p style={{wordWrap:' break-word' ,flex:1, justifyContent: 'center', alignItems: 'center'}}> 
-This project used a weather API to pull weather updates from any area you search </p>
+Track your fitness here with the Fitness Tracker App. Technologies used include: HTML, CSS and Javascript
+ </p>
  <p> <a href='https://github.com/MasonBurke/fitness-tracker'> The Code </a>||
   <a href="https://pure-woodland-71734.herokuapp.com/"> The Site </a> </p>
 
@@ -52,3 +55,7 @@ This project used a weather API to pull weather updates from any area you search
 }
 
 export default Projects
+
+
+//create clickable picture to link to site. add more projects. make desktop responsive.
+//icons for code and site instead of text
